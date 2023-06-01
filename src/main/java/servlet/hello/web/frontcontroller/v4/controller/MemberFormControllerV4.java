66c0f121e78f4e35f28abcc9bec288c94078a20c
haekang <ydjm1027@naver.com>
2023-06-01 21:45:49 +0900

    20230601

---
+++ src/main/java/servlet/hello/web/frontcontroller/v4/controller/MemberFormControllerV4.java
@@ -0,0 +1,14 @@
+package servlet.hello.web.frontcontroller.v4.controller;
+
+import servlet.hello.web.frontcontroller.ModelView;
+import servlet.hello.web.frontcontroller.v3.ControllerV3;
+import servlet.hello.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberFormControllerV4 implements ControllerV4 {
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        return "new-form";
+    }
+}
